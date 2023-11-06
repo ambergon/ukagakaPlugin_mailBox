190078c43e7e7e5f9a465d81b64eee78bcfbb42e
ambergon <sc.foxgon@gmail.com>
2023-11-06 20:00:51 +0900

    [now]
    sqliteを静的リンクするように

--- code/do.bat
+++ code/do.bat
@@ -2,8 +2,9 @@
 
 
 del /q ..\main.dll
-g++ -pthread -shared -o ../main.dll main.cpp -lsqlite3
+g++ -pthread -shared -o ../main.dll main.cpp -static -lsqlite3
 @rem g++ -pthread -shared -o main.dll main.cpp -I ./include  liblua54.a -g
 @rem g++ -shared -o main.dll main.cpp -I ./include  liblua54.a -g
 @rem g++ -o a.exe main.cpp -lsqlite3
 rem ssp /g Ghost_Mine
+rem
