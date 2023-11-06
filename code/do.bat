98c55eecfe9fe8fbb205e3e12aab8d502f24f14b
ambergon <sc.foxgon@gmail.com>
2023-11-06 11:29:51 +0900

    [now]
    ディレクトリ移動

--- code/do.bat
+++ code/do.bat
@@ -1,8 +1,8 @@
 
 
 
-del /q main.dll
-g++ -pthread -shared -o main.dll main.cpp -lsqlite3
+del /q ..\main.dll
+g++ -pthread -shared -o ../main.dll main.cpp -lsqlite3
 @rem g++ -pthread -shared -o main.dll main.cpp -I ./include  liblua54.a -g
 @rem g++ -shared -o main.dll main.cpp -I ./include  liblua54.a -g
 @rem g++ -o a.exe main.cpp -lsqlite3
